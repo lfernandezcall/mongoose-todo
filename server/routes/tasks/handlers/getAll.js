e09a8d007e4759b7262b8b48e8a9e1822e1b4c20
Luis Alberto <fernandezc.alberto@gmail.com>
2017-05-29 15:32:36 +0200

    console logs added to debug

--- server/routes/tasks/handlers/getAll.js
+++ server/routes/tasks/handlers/getAll.js
@@ -1,8 +1,11 @@
 const Task = require('../../../models/Task')
 
 function getAll (req, res) {
+  console.log('requesting all tasks...')
   Task.find()
     .then(data => {
+    	console.log('response received all tasks...')
+    	console.log(data)
       res.render('index', { data })
     })
 }
